66c4b7aa1524f2ff8b5b549670fa18dfea775f96
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-03-07 09:57:08 -0500

    swipper

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*const swiper = new Swiper('.swiper-screenshots', {
+    centeredSlides: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        }, }
+
+    
+  });*/
+
+  var swiper = new Swiper(".swiper-screenshots", {
+    centeredSlides: true,
+    loop: true,
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 200
+        }, }
+  });
