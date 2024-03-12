c6e3d51f68de341f0aae62a89aca3809b5bb0a57
audreydandurand <112189528+audreydandurand@users.noreply.github.com>
2024-03-12 18:22:59 -0400

    ajustement_swiper

--- js/script.js
+++ js/script.js
@@ -19,16 +19,17 @@
   var swiper = new Swiper(".swiper-screenshots", {
     centeredSlides: true,
     loop: true,
-
+    breakpoints: {
+        // when window width is >= 1200px
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 30
+        }, 
+      },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
 
-    breakpoints: {
-        // when window width is >= 320px
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 20
-        }, }
+
   });
